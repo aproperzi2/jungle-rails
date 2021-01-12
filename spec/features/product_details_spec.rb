bd664f8f98042845875cda8711a3e71f4f281472
Luca Properzi <abramoproperzi@gmail.com>
2021-01-12 22:25:30 +0000

    added testing for details page routing

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to ,", type: :feature, js: true do
-
-  #SETUP
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
   before :each do 
     @category = Category.create! name: 'Apparel'
 
@@ -17,14 +15,11 @@ RSpec.feature "Visitor navigates to ,", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
+  scenario "confirm you can click on product details and go to details page" do 
     visit root_path
-
-    # DEBUG / VERIFY
     # save_screenshot
-
-    expect(page).to have_css 'article.product', count: 10
+    first("article.product").find_link("Details").click
+    expect(page).to have_content "Quantity"
+    # save_screenshot
   end
-
 end
